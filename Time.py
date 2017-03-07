99651e619cfaeeaafe37eb3641d31b74ee113746
hejeffery <553504116@qq.com>
2017-03-07 18:32:29 +0800

    完善demo

--- Time.py
+++ Time.py
@@ -5,3 +5,12 @@
 import time
 
 print time.time()
+
+localtime = time.localtime(time.time())
+print localtime
+
+localtime = time.asctime(time.localtime(time.time()))
+print localtime
+
+localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+print localtime
